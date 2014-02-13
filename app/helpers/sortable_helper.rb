02c253061887ff021346d8d35b42c6ecaab7fb49
Manbo- <Manbo-@server.fake>
2014-02-14 00:59:00 +0900

    add sortable and sortable_set

--- app/helpers/sortable_helper.rb
+++ app/helpers/sortable_helper.rb
@@ -1,9 +1,21 @@
 module SortableHelper
+  def sortable(object)
+    @sortable[object]
+  end
 
-  def sortable_fetch(models, &block)
+  def sortable_fetch(models)
     raise "You must call with block!" unless block_given?
     models.each do |object|
       yield object, "#{object.class}_#{object.id}"
     end
+  end
+
+  private
+
+  def sortable_set(models)
+    @sortable ||= models.inject({}) do |sortable, object|
+      sortable.merge(object => "#{object.class}_#{object.id}")
+    end
+    models
   end
 end
